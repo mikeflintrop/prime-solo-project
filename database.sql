9aedcfed928a0cfdb8616981ee2a865ba449253d
Mike Flintrop <mike.flintrop@gmail.com>
2022-08-02 14:03:07 -0500

    updated header/footer, about with minor db setup tweaks

--- database.sql
+++ database.sql
@@ -5,6 +5,16 @@
 -- Otherwise you will have errors!
 CREATE TABLE "user" (
     "id" SERIAL PRIMARY KEY,
-    "username" VARCHAR (80) UNIQUE NOT NULL,
-    "password" VARCHAR (1000) NOT NULL
+    "username" VARCHAR (255) UNIQUE NOT NULL,
+    "password" VARCHAR (255) NOT NULL,
+    "clearance_level" INTEGER NOT NULL DEFAULT 1
 );
+
+CREATE TABLE "workout_history" (
+    "id" SERIAL PRIMARY KEY,
+    "user_id" INTEGER REFERENCES "user" UNIQUE NOT NULL,
+    "duration" INTEGER,
+    "date" DATE NOT NULL,
+    "notes" VARCHAR (100)
+);
+

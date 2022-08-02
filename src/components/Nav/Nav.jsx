aac87891d8b1aea4e83f4d1570932afa477fa878
Mike Flintrop <mike.flintrop@gmail.com>
2022-08-02 16:09:27 -0500

    added exercises route, server, reducer, saga, and added currentworkout comp

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -32,6 +32,10 @@ function Nav() {
               Info Page
             </Link>
 
+            <Link className="navLink" to="/current">
+              Current Workout
+            </Link>
+
             <LogOutButton className="navLink" />
           </>
         )}

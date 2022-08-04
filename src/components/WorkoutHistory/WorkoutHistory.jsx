88a30e0b9d74973c959c8aa528484781eb88559b
Mike Flintrop <mike.flintrop@gmail.com>
2022-08-04 10:05:08 -0500

    added format for history display

--- src/components/WorkoutHistory/WorkoutHistory.jsx
+++ src/components/WorkoutHistory/WorkoutHistory.jsx
@@ -1,5 +1,7 @@
-import React, { useState } from 'react';
-import {useSelector} from 'react-redux';
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useHistory, useParams } from 'react-router-dom';
+import axios from 'axios';
 
 // Basic functional component structure for React with default state
 // value setup. When making a new component be sure to replace the
@@ -10,11 +12,30 @@ function WorkoutHistory(props) {
     const store = useSelector((store) => store);
     const [heading, setHeading] = useState('Workout History');
 
+    let id = useParams();
+
+    console.log('this is user id', id);
+
+
+    useEffect(() => {
+        dispatch({ type: "FETCH_HISTORY", payload: id });
+    }, []);
+
     return (
         <div>
-        <h2>{heading}</h2>
+            <h2>{heading}</h2>
+                {history?.map((item) => {
+                    return (
+                        <div key={item.id} >
+                            <h3>Workout Date: {item.date}</h3>
+                            <h3>Workout Duration: {item.duration}</h3>
+                            <h3>Workout Notes: {item.notes}</h3>
+                            <button>Edit</button>
+                            <button>Delete</button>
+                        </div>
+                    );
+                })}
 
-        
         </div>
     );
 }

95b98e14c853427d7941758b805be0f464ec261a
Mike Flintrop <mike.flintrop@gmail.com>
2022-08-10 17:52:14 -0500

    added about header

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 // This is one of our simplest components
 // It doesn't have local state,
@@ -6,8 +6,11 @@ import React from 'react';
 // or even care what the redux state is'
 
 function AboutPage() {
+  const [heading, setHeading] = useState('About');
+
   return (
     <div className="container">
+        <h2>{heading}</h2>
       <div>
         <div>
           <img src="images/steelcore_main.jpeg" alt="steelcore_main" />

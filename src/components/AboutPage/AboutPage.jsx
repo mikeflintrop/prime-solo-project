48cb7368e7f4f9a91e97255939ae1d5c66982ec7
Mike Flintrop <mike.flintrop@gmail.com>
2022-08-11 22:21:32 -0500

    added about css and updated the about page

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import './AboutPage.css';
 
 // This is one of our simplest components
 // It doesn't have local state,
@@ -16,24 +17,25 @@ function AboutPage() {
           <img src="images/steelcore_main.jpeg" alt="steelcore_main" />
         </div>
           <h3>Next Steps</h3>
-            <ul>
+            <ul className="noBullets">
               <li>Expand the types of workouts</li>
             </ul>
           <h3>Tech Used</h3>
-            <ul>
+            <ul className="noBullets">
               <li>React</li>
               <li>Redux</li>
               <li>Redux-Saga</li>
               <li>Node</li>
               <li>Express</li>
               <li>Postgres</li>
+              <li>Exercise DB API</li>
               <li>Material UI</li>
               <li>Sweet Alerts 2</li>
               <li>Date-fns</li>
             </ul>
-          <h3>Thank You</h3>
-            <ul>
-              <li>My Family and Friends</li>
+          <h3>Thanks</h3>
+            <ul className="noBullets">
+              <li>Friends and Family</li>
               <li>Jemisin Cohort</li>
               <li>Instructor Liz</li>
             </ul>

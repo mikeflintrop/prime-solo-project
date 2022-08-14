b17242f2988a2f79a9914172847152bde7a9298b
Mike Flintrop <mike.flintrop@gmail.com>
2022-08-14 18:29:24 -0500

    updated snackbar layout

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -4,10 +4,34 @@ import { useHistory } from 'react-router-dom';
 
 import * as React from 'react';
 import Button from '@mui/material/Button';
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
+
+
 
 
 function LoginPage() {
+
+  const Alert = React.forwardRef(function Alert(props, ref) {
+    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+  });
+
   const history = useHistory();
+
+  const [open, setOpen] = React.useState(false);
+
+  // const handleClick = () => {
+  //   setOpen(true);
+  // };
+
+  const handleClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+
+    setOpen(true);
+  };
+
 
   return (
     <div>
@@ -27,6 +51,14 @@ function LoginPage() {
         >
           Register
         </Button>
+        <Snackbar open={true} autoHideDuration={3000} onClose={(event)=>{handleClose}}>
+
+          <Alert onClose={(event)=>{handleClose}} severity="info" sx={{ width: '100%' }}>
+            Login to start a workout!
+          </Alert>
+
+        </Snackbar>
+
       </center>
     </div>
   );
